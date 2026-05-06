7d34ba6416297020eb2e1c88cb030c401400c19f
Nguyễn Hoàng Tùng <tungnh0308@gmail.com>
2026-05-06 19:16:55 +0700

    xoa may cai thua thai~

--- AuthService/src/test/resources/sql/integration-clean-users-roles.sql
+++
@@ -1,7 +0,0 @@
--- Xóa dữ liệu phụ thuộc user/role trước khi test seeder (chạy transaction ISOLATED, commit).
-SET FOREIGN_KEY_CHECKS = 0;
-DELETE FROM order_items WHERE 1=1;
-DELETE FROM orders WHERE 1=1;
-DELETE FROM users WHERE 1=1;
-DELETE FROM roles WHERE 1=1;
-SET FOREIGN_KEY_CHECKS = 1;
